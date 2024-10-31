ef194f1b6f5d4da37f3f37a9904db18f56df967b
Your Name <benekmasny61@gmail.com>
2024-10-31 10:07:15 +0100

    changed content

--- src/Kalkulator.jsx
+++ src/Kalkulator.jsx
@@ -30,12 +30,16 @@ const Calculator = () => {
         <div className="result">{result}</div>
       </div>
       <div className="buttons">
-        <button onClick={() => handleClick("1")}>1</button>
+        <h1>LELELEELELEELELE</h1>
+        <br />
         <button onClick={() => handleClick("2")}>2</button>
         <button onClick={() => handleClick("3")}>3</button>
+
         <button onClick={() => handleClick("+")}>+</button>
         <button onClick={() => handleClick("4")}>4</button>
+        <h2>zuzek</h2>
         <button onClick={() => handleClick("5")}>5</button>
+        <h3>masnybenek</h3>
         <button onClick={() => handleClick("6")}>6</button>
         <button onClick={() => handleClick("-")}>-</button>
     
